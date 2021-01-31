2ceea95b26a23a82584e065c940a78b7e0a1e5b7
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-31 17:44:32 -0500

    Created an images folder inside public and added images. Added images and social links in the app.

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import Navbar from '../components/navbar';
+import Jumbotron from '../components/jumbotron';
+import Card from '../components/card';
+import Footer from '../components/footer';
+// import css for images
 
-const Contact = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default Contact;
+const App = () => (
+  <div>
+    <Navbar />
+    <Jumbotron phrase="Connect with Me" subtitle="Contact and links" />
+    <hr />
+    <div className="row">
+      <div className="col contactCol">
+        <Card source="../public/images/email.png" text="" />
+        <Card source="../public/images/github.png" text={<a href="https://github.com/Valkimani" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">GitHub</button></a>} />
+        <Card source="../public/images/linkedin.png" text={<a href="https://www.linkedin.com/in/val-kimani-/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">LinkedIn</button></a>} />
+        <Card
+        //   source="resume.png"
+          text={
+            <React.Fragment>
+              <a href="#resumepdf" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark">Resume</button></a>
+              <a href="#resumepdf" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-dark"><FontAwesomeIcon icon={faDownload} /></button></a>
+            </React.Fragment>
+          }
+        />
+        <Card source="../public/images/cellphone.png" text="470 839 5079" />
+      </div>
+    </div>
+    <Footer />
+  </div>
+);
+export default App;
