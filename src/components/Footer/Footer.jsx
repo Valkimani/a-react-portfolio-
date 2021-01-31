35afc5328cdd783f93f1c52e183caa6251e71335
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-31 18:01:02 -0500

    Added footer content in the footer jsx. Added a paragrah and exported the footer.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+    <footer className="footer">
+      <div className="container">
+        <p className="text-muted text-center">Val Kimani 2021</p>
+      </div>
+    </footer>
+  );
+  export default Footer;
