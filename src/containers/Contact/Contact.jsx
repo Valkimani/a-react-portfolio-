85f2ee275bc3e420c5e48173a8c327f1117fe730
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-31 17:57:34 -0500

    Added a proptype to the jumbotron.jsx. Incldued a h1 and paragraph for the subtitle and phrase.

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import Navbar from '../components/navbar';
-import Jumbotron from '../components/jumbotron';
-import Card from '../components/card';
-import Footer from '../components/footer';
+// import jumbotron
+// import card
+// import footer
 // import css for images
 
 
