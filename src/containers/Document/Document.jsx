43ae331d1d7d325dbe16df69d9ed54e845e24c0e
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-31 19:52:35 -0500

    Created a viewRoutes.js files. Added a html boiler plate in document.jsx. Added an export default for the document.

--- src/containers/Document/Document.jsx
+++ src/containers/Document/Document.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import Document, {
+    Head,
+    Main,
+    NextScript,
+  } from 'next/document';
 
-const document = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default document;
+  
+  export default class MainDocument extends Document {
+    static async getInitialProps(ctx) {
+      const initialProps = await Document.getInitialProps(ctx);
+      return { ...initialProps };
+    }
+    render() {
+      return (
+        <html lang="en">
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </html>
+      );
+    }
+  }
+  
